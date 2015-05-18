e5946a3b9af9b4ff10636dfd908275c9b7cef436
Elizabeth <boutbitnz2@gmail.com>
2015-05-17 23:34:40 -0500

    change background

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
 $(document).ready(function() {
-	// $(".herb-detail").hide();
-	// $(".veggie-detail").hide();
-
+	$(".herb-detail").hide();
+	$(".veggie-detail").hide();
 	$("#veggies").hide();
 	$("#herbs").hide();
+	
 	$("#herbs-button").on("click", function(event){
 		event.preventDefault();
 		$("#herbs").show();
 		$("#veggies").hide();
-
-
 	});
+
+
 	$("#veggies-button").on("click", function(event){
 		event.preventDefault();
 		$("#veggies").show();
 		$("#herbs").hide();
 	});
+
+	$(".cilantro h1").on("click", function(event){
+		event.preventDefault();
+		$(".cilantro-detail").show();
+	});
+
+
+	
 });
 
