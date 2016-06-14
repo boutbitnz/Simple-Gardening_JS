93a216c24f2ac80a10f2cb25c5a83485ae3abac5
boutbitnz <ebell@flexioln.us>
2016-06-13 23:05:46 -0500

    Reconstrduct/JS code/cleanup

--- js/script.js
+++ js/script.js
@@ -1,46 +1,54 @@
 $(document).ready(function() {
+	// hide these by default with styles
 	$(".herb-detail").hide();
 	$(".veggie-detail").hide();
 	$("#veggies").hide();
 	$("#herbs").hide();
 	$("#home").hide();
 	$("main").hide();
-	
-	$("#herbs-button").on("click", function(event){
+
+	$("#herbs-button").on("click", herbButtonFunc);
+	$("#veggies-button").on("click", vegButtonFunc);
+	$("#herbs article h1").on("click", toggHerbDetailFunc);
+	$("#veggies article h1").on("click", toggVeggieDetailFunc);
+	$("#home").on( "click", homeButtonFunc);
+
+	function herbButtonFunc() {
 		// event.preventDefault();
+		console.log(arguments)
 		$("#herbs").show();
 		$("#veggies").hide();
 		$("main").show();
-	});
+	}
 
-
-	$("#veggies-button").on("click", function(event){
+	function vegButtonFunc() {
 		// event.preventDefault();
+		console.log("arguments");
 		$("#veggies").show();
 		$("#herbs").hide();
 		$("main").show();
-	});
+	}
 
-	
-	$("#herbs article h1").on("click", function(event){
+	function toggHerbDetailFunc() {
 		// event.preventDefault();
 		$("#header").hide();
 		$(".herb-detail").hide();
 		$(".veggie-detail").hide();
 		$("#home").show();
 		$(this).closest("article").find(".herb-detail").fadeToggle( "slow", "linear");
-		
-	});
-	$("#veggies article h1").on("click", function(event){
+
+	}
+
+	function toggVeggieDetailFunc(){
 		// event.preventDefault();
 		$("#header").hide();
 		$(".herb-detail").hide();
 		$(".veggie-detail").hide();
 		$(this).closest("article").find(".veggie-detail").fadeToggle( "slow", "linear");
 		$("#home").show();
-	}); 
+	}
 
-	$("#home").on( "click", function() {
+	function homeButtonFunc() {
 		$(".herb-detail").hide();
 		$(".veggie-detail").hide();
 		$("#herbs").hide();
@@ -48,8 +56,17 @@ $(document).ready(function() {
 		$("#home").hide();
 		$("#header").show();
 		$("main").hide();
+	}
 
-	});
-	
 });
+
+
+
+
+
+
+
+
+
+
 
